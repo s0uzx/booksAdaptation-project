4d042d306bf5cff7ff7af5b90944287220ab349e
Sara Souza <sarasouza.tds2.senai@gmail.com>
2025-06-17 13:44:27 -0300

    feat: create livro model

--- src/models/livroModel.js
+++ src/models/livroModel.js
@@ -1,2 +1,46 @@
 import prisma from "../../prisma.js";
+ 
+class LivroModel {
+    async findAll() {
+        try {
+            const livros = await prisma.livro.findMany({
+                include: {
+                    adaptacoes: true,
+                },
+                orderBy: {
+                    createdAt: "desc",
+                },
+            });
 
+            return livros;
+        } catch (error) {
+            console.error("Erro ao encontrar livros:", error);
+            throw error;
+        }
+    }
+
+    async create(titulo, autor, anoPublicacao, genero, sinopse, paginas) {
+        try {
+            const novoLivro = await prisma.livro.create({
+                data: {
+                    titulo,
+                    autor,
+                    anoPublicacao,
+                    genero,
+                    sinopse,
+                    paginas,
+                },
+                include: {
+                    adaptacoes: true,
+                },
+            });
+
+            return novoLivro;
+        } catch (error) {
+            console.error("Erro ao criar livro:", error);
+            throw error;
+        }
+    }
+}
+
+export default new LivroModel();
